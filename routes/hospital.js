537d23cecd548cfdcb4e8a170bd60b146fe50f19
Almilkar <almilkar@gmail.com>
2018-03-21 14:19:53 +0100

    Altas GET y POST

--- routes/hospital.js
+++ routes/hospital.js
@@ -1,48 +1,76 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var readline = require('readline');
 
 // route -> /hospital
+
+var fileDBHospital = "/dbfiles/altaHospitalDB.txt";
 
 router.get('/', function(req, res,) {
   	res.send('Rutas hospital /');
 });
 
-// http://localhost:3000/html/frmAltasHospital/pAltasHospital.html?
 router.get('/registro', function(req, res, next) { 
-	var solicitudAltaHospital = "Hola solicitud de alta del hospital";  //req.body; 
-   	
+	var solicitudAltaHospital = "Hola solicitud de alta del hospital";  //req.body;
+   	// --
 	var {nombre, apellido1, apellido2, numDNI, numSS, fecha, nDolenciasCHK, Dias, ultimoingreso} = req.query;
-
-	console.log(nombre);
-	console.log(apellido1);
-	console.log(apellido2);
-	console.log(numDNI);
-	console.log(numSS);
-	console.log(fecha);
-	console.log(nDolenciasCHK);
-	console.log(Dias);
-	console.log(ultimoingreso);
-
+	var uuid = crearUUID();
+	var reg = uuid + "^" + nombre + "^" + apellido1 + "^" + apellido2 + "^" + numDNI + "^" + numSS + "^" + fecha + "^" + nDolenciasCHK + "^" + Dias + "^" + ultimoingreso;
+	fs.appendFile(fileDBHospital, reg, function (err) {
+  		if (err) throw err;
+  		console.log('Agregado registro');
+	});
+	// --
   	res.send(solicitudAltaHospital);
 });
 
 router.post('/registro', function(req, res, next) { 
-	var solicitudAltaHospital = "Hola solicitud de alta del hospital";  //req.body; 
-   	
+	var solicitudAltaHospital = "Hola solicitud de alta del hospital";  //req.body;
+   	// --
 	var {nombre, apellido1, apellido2, numDNI, numSS, fecha, nDolenciasCHK, Dias, ultimoingreso} = req.body;
-
-	console.log(nombre);
-	console.log(apellido1);
-	console.log(apellido2);
-	console.log(numDNI);
-	console.log(numSS);
-	console.log(fecha);
-	console.log(nDolenciasCHK);
-	console.log(Dias);
-	console.log(ultimoingreso);
-
+	var uuid = crearUUID();
+	var reg = uuid + "^" + nombre + "^" + apellido1 + "^" + apellido2 + "^" + numDNI + "^" + numSS + "^" + fecha + "^" + nDolenciasCHK + "^" + Dias + "^" + ultimoingreso;
+	fs.appendFile(fileDBHospital, reg, function (err) {
+  		if (err) throw err;
+  		console.log('Agregado registro');
+	});
+	// --
   	res.send(solicitudAltaHospital);
 });
 
+////////////////////////////////    Aux Ini   /////////////////////////
 
+function crearUUID() {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+function leeNumRegistros(archivo) {
+
+fs.open(archivo, 'rs', function(error, fd) {
+
+	// --
+	var rlinea = readline.createInterface({
+	  input: fs.createReadStream(archivo);
+	});
+
+	var numlineas = 0;
+	rlinea.on('line', function (linea) {
+	  numlineas++;
+	  console.log('Line number ' + numlineas + ': ' + linea);
+	});
+	// --
+}
+
+	return numlineas;
+}
+
+
+////////////////////////////////   Aux Fin   /////////////////////////
 module.exports = router;
